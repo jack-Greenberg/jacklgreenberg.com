9343f6e7e4922ae8326eda46865f16d08cef2c41
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-03-01 10:40:50 -0800

    Add modular sections

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php phpinfo(); ?>
+<?php include('./head.php') ?>
+
+<?php include('./_includes/header.php') ?>
+<?php include('./_includes/projects.php') ?>
+<?php include('./_includes/resume.php') ?>
+
+<?php include('./_includes/footer.php') ?>
+
+<?php include('./foot.php') ?>
