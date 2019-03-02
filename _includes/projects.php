f16d77bbaae146c6672157032f7a7fdcf76db81b
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-03-01 16:53:12 -0800

    Style projects

---
+++ _includes/projects.php
@@ -0,0 +1,20 @@
+<section class="projects">
+    <button class="projects-expand  js-projects-expand"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></button>
+    <h2 class="projects__title">Projects</h2>
+    <span class="line"></span>
+    <p class="projects__subtitle">Code I’ve been writing and other things occupying my time</p>
+
+    <ul class="projects__list">
+        <li class="project-item">
+            <h3 class="project-item__title">Built By Women</h3>
+            <p class="project-item__description">Coded and assisted in design for a project for the Beverly Willis Architecture Foundation, who will be drafting a map in 2020 of buildings architected by women in Los Angeles.</p>
+            <a class="project-item__link" href="https://builtbywomen.us">builtbywomen.us</a>
+        </li>
+
+        <li class="project-item">
+            <h3 class="project-item__title">Passphrase Generator</h3>
+            <p class="project-item__description">A passphrase generator built in React with a clean and useable UI.</p>
+            <a class="project-item__link" href="https://tools.jacklgreenberg.com/passphrase">tools.jacklgreenberg.com /passphrase</a>
+        </li>
+    </ul>
+</section>
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php include('./head.php') ?>
 
-<?php include('./_includes/header.php') ?>
-<?php include('./_includes/projects.php') ?>
-<?php include('./_includes/resume.php') ?>
+<div class="site-container js-site-container">
+    <?php include('./_includes/header.php') ?>
 
-<?php include('./_includes/footer.php') ?>
+    <?php include('./_includes/projects.php') ?>
+    <?php include('./_includes/resume.php') ?>
+
+    <?php include('./_includes/footer.php') ?>
+</div>
 
 <?php include('./foot.php') ?>
