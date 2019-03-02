40c9ad96faf7b128d39820a923e14aa687774102
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-03-01 19:55:15 -0800

    Add footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,3 +1,11 @@
-<footer>
-    
+<footer class="footer">
+    <p class="footer__copyright">(c) 2019 Jack Greenberg</p>
+    <br>
+    <ul class="footer__social-links">
+        <li><a href="">Credits</a></li>
+        <li><a href="https://github.com/jack-Greenberg/">Github</a></li>
+        <li><a href="https://instagram.com/greenberg__">Instagram</a></li>
+    </ul>
+    <!-- <br> -->
+    <!-- <p class="redesign">This is the <span class="js-redesign"></span> time I've redesigned my site</p> -->
 </footer>
