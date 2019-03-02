208850a6e884bc9bc7b6c90207468ae7310035e0
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-03-01 18:20:20 -0800

    Add section break

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 import * as d3 from 'd3';
 import textures from 'textures';
+
+var svg = d3.select('.js-texture-break')
+    .append('svg')
+    .attr('viewBox', '0 0 100 50')
+    .style('display', 'block');
+
+var t = textures.lines()
+  .thicker()
+  .lighter()
+  .stroke('#dacaee');
+
+svg.call(t);
+
+svg.append('rect')
+	.style('fill', t.url())
+    .style('width', '100%')
+    .style('height', '100%');
