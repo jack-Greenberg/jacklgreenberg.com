1ff96c8ad62cfdc1279cd54bd936efdf024fa735
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-03-04 23:48:59 -0800

    Back button reverts from "subdir" to home
    There is still an issue with clicking the back issue when in the nav 
    menu

--- js/index.js
+++ js/index.js
@@ -4,19 +4,22 @@ import textures from 'textures';
 var svg = d3.select('.js-texture-break')
     .append('svg')
     .attr('viewBox', '0 0 100 50')
-    .style('display', 'block');
+    .style('display', 'block')
+;
 
 var t = textures.lines()
-  .thicker()
-  .lighter()
-  .stroke('#dacaee');
+    .thicker()
+    .lighter()
+    .stroke('#dacaee')
+;
 
 svg.call(t);
 
 svg.append('rect')
 	.style('fill', t.url())
     .style('width', '100%')
-    .style('height', '100%');
+    .style('height', '100%')
+;
 
 ///////
 
@@ -42,13 +45,15 @@ var ul = d3.select('.js-chart__legend')
         .append('li')
         .text(function(d) {
             return d;
-        });
+        })
+;
 
 
 var svg = d3.select('.js-chart__graph')
     .append('svg')
     .style('width', '100%')
-    .style('height', '160px');
+    .style('height', '160px')
+;
 
 svg.selectAll('rect')
     .data(skillNumbers)
@@ -65,41 +70,37 @@ svg.selectAll('rect')
         .attr('x', 0)
         .style('opacity', function(d) {
             return d/100 * 1.25;
-        });
+        })
+    ;
 
 $('.nav-button').on('click', function() {
     $('body').toggleClass('show-nav');
-    // if ($('body').hasClass('show-nav')) {
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: '_includes/nav.php',
-    //         success: function(data) {
-    //             $('.expanded-section-overlay').append(data);
-    //         }
-    //     });
-    // } else {
-    //     $('.expanded-section-overlay').empty();
-    // };
 });
 
+// The expand-section button
 $('.js-button-expand').on('click', function(e) {
     var clicked = $(e.target);
-
     var section = clicked.closest('section').attr('class');
-    console.log(section);
 
-    history.pushState({
-        page: section,
-    }, null, '/' + section);
+    history.pushState({page: section}, null, '/' + section);
     document.title = 'Jack Greenberg | ' + section;
-
-    // window.onpopstate = function(e) {
-    //     console.log(history.state.page);
-    // };
-
     $('body').addClass(section + '--expanded');
 });
 
+// Overrides the back button, and uses it to remove the section
+$(window).on('popstate', function(e) {
+    var s = e.originalEvent.state;
+
+    if (s == null) {
+        history.pushState(null, null, '/');
+        document.title = 'Jack Greenberg';
+        $('body').removeClass(function(i, className) {
+            return (className.match(/(.)*\-\-expanded*/) || []).join(' ') + ' show-nav';
+        });
+    };
+});
+
+// The 'close section' button on the page (<-)
 $('.js-close-section-button').on('click', function() {
     $('body').removeClass(function(i, className) {
         return (className.match(/(.)*\-\-expanded*/) || []).join(' ') + ' show-nav';
