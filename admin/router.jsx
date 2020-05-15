e5bb19ed7875f4814047e96c9f7e37ff56c60f88
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-05-15 14:51:24 -0700

    Change archetypes to content

--- admin/router.jsx
+++ admin/router.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from "react-router-dom";
 
-import { Archetypes } from './pages/archetypes.jsx';
-import { Archetype } from './pages/archetype.jsx';
+import { Home } from './pages/home.jsx';
+import { Content } from './pages/content.jsx';
 import { Sidebar } from './partials/sidebar.jsx';
 
 export class Router extends React.Component {
@@ -10,18 +10,16 @@ export class Router extends React.Component {
         super(props);
     }
     render() {
-        var archetypeRoutes = this.props.archetypes.map(a => {
-            return <Route path={"/" + a.name.toLowerCase()} render={(props) => <Archetype name={a.name} />} />;
+        var contentRoutes = this.props.archetypes.map(a => {
+            return <Route path={"/" + a.name.toLowerCase()} render={(props) => <Content name={a.name} />} />;
         });
 
         return (
             <HashRouter>
                 <Sidebar />
                 <Switch>
-                    {archetypeRoutes}
-                    <Route path="/archetypes/:archetype" render={(props) => <div>Test</div>} />
-                    <Route path="/archetypes" render={(props) => <Archetypes {...props} />} />
-                    {/*<Route path="/login/" render={(props) => <Login {...props} />} />*/}
+                    <Route exact path="/" render={(props) => <Home />} />
+                    { contentRoutes }
                 </Switch>
             </HashRouter>
         )
